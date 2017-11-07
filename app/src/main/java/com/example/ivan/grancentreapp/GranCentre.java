c4b61aabb6c12a3f69c20c11f12629c1c476ee69
ivansoriabia <ivansoriabia@gmail.com>
2017-11-07 20:55:50 +0100

    ...

--- app/src/main/java/com/example/ivan/grancentreapp/GranCentre.java
+++ app/src/main/java/com/example/ivan/grancentreapp/GranCentre.java
@@ -1,13 +1,21 @@
 package com.example.ivan.grancentreapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class GranCentre extends AppCompatActivity {
+public class GranCentre extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gran_centre);
     }
+
+    protected void restaurants (View v){
+        Intent restaurants = new Intent(GranCentre.this, restaurants.class);
+        startActivity(restaurants);
+    }
 }
+
